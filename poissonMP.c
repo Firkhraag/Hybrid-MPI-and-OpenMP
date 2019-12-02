c01fc6bb0eb3e5816b4ecfa00cfd98d57818a14c
Firkhraag <firkhraag@gmail.com>
2019-12-02 14:45:00 +0300

    Update

--- poissonMP.c
+++ poissonMP.c
@@ -58,7 +58,7 @@ int main(int argc, char **argv) {
     const float eps = 1e-5;
 
     // Square grid
-    const int n = 5;
+    const int n = 6;
 
     // Step
     const float stepX = (a2 - a1) / n;
